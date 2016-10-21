a03a7214989dfe5fd9edf2340e16422ed0e84422
Manu Bamba <mbamba@xebia.com>
2016-10-21 19:11:53 +0530

    Added toggle to feeds

--- App.js
+++ App.js
@@ -4,24 +4,43 @@ import Immutable from 'immutable';
 import Tweet from './tweet';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import Toggle from 'material-ui/Toggle';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.io = io('http://localhost:3000/');
     this.io.on('tweet', (tweet) => {
-        this.setState({
-            tweets: this.state.tweets.unshift(tweet) 
-        });
+        if (this.state.fetchTweets) {
+            this.setState({
+                tweets: this.state.tweets.unshift(tweet)
+            });
+        }
     })
   }
 
   state = {
-    tweets: Immutable.List()
+    tweets: Immutable.List(),
+    fetchTweets: true
+  }
+
+  toggleFeed() {
+    this.setState({
+        fetchTweets: !this.state.fetchTweets
+    });
   }
 
   render() {
-    const {tweets} = this.state;
+    const {tweets, fetchTweets} = this.state;
+    const styles = {
+      labelStyle: {
+        color: '#444'
+      },
+      toggle: {
+        margin: 25,
+        width: 200
+      }
+    };
     return (
     <MuiThemeProvider>
       <div>
@@ -29,6 +48,17 @@ export default class App extends React.Component {
             title="React Twitter"
             iconClassNameRight="muidocs-icon-navigation-expand-more"
           />
+          <Toggle
+              defaultToggled={true}
+              label="Fetch Tweets"
+              labelStyle={styles.labelStyle}
+              onToggle={() => this.toggleFeed()}
+              thumbStyle={styles.thumbOff}
+              toggled={fetchTweets}
+              trackStyle={styles.trackOff}
+              trackSwitchedStyle={styles.trackSwitched}
+              style={styles.toggle}
+            />
           {tweets.map(tweet =>
         <Tweet key={tweet.id_str} {...tweet} />
       )}</div>
